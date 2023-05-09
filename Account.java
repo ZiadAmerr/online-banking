c75d0e486bb01d95973387fad674ff8d39c031fd
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-05-10 01:19:47 +0300

    Added basic implementations

--- Account.java
+++ Account.java
@@ -1,25 +1,46 @@
 import java.util.List;
 
 public class Account {
-    private int account_number;
+    private static int account_number_counter = 0;
+    private final int account_number;
     private double balance;
     private List<Transaction> transaction_history;
-    private User owner;
+    private final User owner;
+
+    public Account(User owner) {
+        account_number = account_number_counter++;
+        balance = 0.0;
+        transaction_history = null;
+        this.owner = owner;
+    }
 
     public void deposit(double amount) {
+        balance += amount;
     }
 
-    public void withdraw(double amount) {
+    public boolean withdraw(double amount) {
+        double new_balance = balance - amount;
+        if (new_balance < 0) {
+            return false;
+        } else {
+            balance = new_balance;
+            return true;
+        }
     }
 
-    public void transfer_money(double amount, Account to_account) {
+    public boolean transfer_money(double amount, Account to_account) {
+        if (withdraw(amount)) {
+            to_account.deposit(amount);
+            return true;
+        }
+        return false;
     }
 
     public double get_balance() {
-        return 0.0;
+        return balance;
     }
 
     public List<Transaction> view_statements() {
-        return null;
+        return transaction_history;
     }
 }
