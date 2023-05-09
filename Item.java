f1c4bf01eb44627e469b0c866952d2c698f1f150
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-05-10 01:43:52 +0300

    Added more methods and basic assertions

--- Item.java
+++ Item.java
@@ -38,5 +38,58 @@ public class Item {
     public Account getAccount() {
         return account;
     }
+    public boolean purchase(Account account) {
+        if (wasPurchased) {
+            return false;
+        }
+        if (account.withdraw(price)) {
+            this.account = account;
+            datePurchased = LocalDateTime.now();
+            wasPurchased = true;
+            return true;
+        }
+        return false;
+    }
+    public String getType() {
+        return type.toString();
+    }
+
+    private static void main(String[] args) {
+        // Create a new item object
+        Item item = new Item(Item.ItemType.ITEM, 10.00, "Test Item");
+        Item bill = new Item(Item.ItemType.BILL, 200.00, "Test Item");
+
+
+        // Test the getId() method
+        assert item.getId() == 0 : "getId() test failed";
+        assert bill.getId() == 1 : "getId() test failed";
+
+        // Test the getName() method
+        assert item.getName().equals("Test Item") : "getName() test failed";
+
+        // Test the getPrice() method
+        assert item.getPrice() == 10.00 : "getPrice() test failed";
+
+        // Test the getDatePurchased() method before purchase
+        assert item.getDatePurchased() == null : "getDatePurchased() test failed";
+
+        // Test the getAccount() method before purchase
+        assert item.getAccount() == null : "getAccount() test failed";
+
+        // Test the purchase() method with sufficient funds
+//        Account account = new Account(20.00);
+//        boolean result = item.purchase(account);
+//        assert result == true : "purchase() test failed";
+//        assert item.getAccount() == account : "purchase() test failed";
+//        assert item.getDatePurchased() != null : "purchase() test failed";
+//
+//        // Test the purchase() method with insufficient funds
+//        account = new Account(5.00);
+//        result = item.purchase(account);
+//        assert result == false : "purchase() test failed";
+//
+//        // Test the getType() method
+//        assert item.getType().equals("ITEM") : "getType() test failed";
+    }
 }
 
