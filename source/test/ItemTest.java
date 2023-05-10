33dc6edddfd76e25b1025b0070f86ea49e03dace
Ahmed Tarek <7madak10@gmail.com>
2023-05-10 19:08:58 +0300

    Completed tests for Item Class

--- source/test/ItemTest.java
+++ source/test/ItemTest.java
@@ -1,12 +1,15 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+
 class ItemTest {
     private static Item item0;
     private static Item item1;
     @BeforeAll
     static void init(){
         item0 = new Item(Item.ItemType.ITEM,5,"pepsi");
-        item1 = new Item(Item.ItemType.BILL,400,"fees");
+        item1 = new Item(Item.ItemType.BILL,99999,"fees");
     }
     @Test
     void getId() {
@@ -23,31 +26,46 @@ class ItemTest {
     @Test
     void getPriceCaseNormal() {
         assertEquals(5,item0.getPrice());
-        assertEquals(400,item1.getPrice());
+        assertEquals(99999,item1.getPrice());
     }
 
     @Test
     void itemWithNegativePrice(){
         assertThrows(IllegalArgumentException.class ,()->
-        { new Item(Item.ItemType.ITEM,-23,"water");});
+                new Item(Item.ItemType.ITEM,-23,"water"));
     }
 
     @Test
     void getDatePurchased() {
-
+        User user = new User("Bruce","batman","ilovecatwoman");
+        Account account = new Account(user);
+        account.deposit(5000);
+        item0.purchase(account);
+        assertEquals(LocalDateTime.now(),item0.getDatePurchased());
     }
 
     @Test
     void getAccount() {
+        User user = new User("Bruce","batman","ilovecatwoman");
+        Account account = new Account(user);
+        account.deposit(5000);
+        item0.purchase(account);
+        assertEquals(account,item0.getAccount());
     }
 
     @Test
     void purchase() {
+        User user = new User("Bruce","batman","ilovecatwoman");
+        Account account = new Account(user);
+        account.deposit(5000);
+        assertTrue(item0.purchase(account));
+        assertFalse(item1.purchase(account));
+
     }
 
     @Test
     void getType() {
-        assertEquals(Item.ItemType.ITEM,item0.getType());
-        assertEquals(Item.ItemType.BILL,item1.getType());
+        assertEquals("ITEM",item0.getType());
+        assertEquals("BILL",item1.getType());
     }
 }
