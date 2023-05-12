5902091f72ae506733f5c79c45b3c76dd3ad1172
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-05-13 00:36:42 +0300

    Added more accurate and comprehensive tests

--- source/test/ItemTest.java
+++ source/test/ItemTest.java
@@ -1,3 +1,6 @@
+package source.test;
+import source.src.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
@@ -8,62 +11,71 @@ class ItemTest {
     private static Item item0;
     private static Item item1;
     @BeforeAll
-    static void init(){
-        item0 = new Item(Item.ItemType.ITEM,5,"pepsi");
-        item1 = new Item(Item.ItemType.BILL,99999,"fees");
+    static void init() {
+        item0 = new Item("Pepsi",5, Item.ItemType.ITEM);
+        item1 = new Item("Fees",99999, Item.ItemType.BILL);
     }
     @Test
     void testGetId() {
-        assertEquals(0,item0.getId());
-        assertEquals(1,item1.getId());
+        assertEquals(0, item0.getId());
+        assertEquals(1, item1.getId());
     }
-
     @Test
     void testGetName() {
-        assertEquals("pepsi",item0.getName());
-        assertEquals("fees",item1.getName());
+        assertEquals("Pepsi", item0.getName());
+        assertEquals("Fees", item1.getName());
     }
-
+    @Test
+    void getType() {
+        Assertions.assertEquals(Item.ItemType.ITEM, item0.getType());
+        Assertions.assertEquals(Item.ItemType.BILL, item1.getType());
+    }
     @Test
     void testGetPrice() {
-        assertEquals(5,item0.getPrice());
-        assertEquals(99999,item1.getPrice());
+        assertEquals(5, item0.getPrice());
+        assertEquals(99999, item1.getPrice());
         assertThrows(IllegalArgumentException.class ,()->
-                new Item(Item.ItemType.ITEM,-23,"water"));
+                new Item("Water", -23, Item.ItemType.ITEM));
     }
-
-
-    @Test
-    void getDatePurchased() {
-        User user = new User("Bruce","batman","ilovecatwoman");
-        Account account = new Account(user);
-        account.deposit(5000);
-        item0.purchase(account);
-        assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS),item0.getDatePurchased().truncatedTo(ChronoUnit.SECONDS));
-    }
-
-    @Test
-    void getAccount() {
-        User user = new User("Bruce","batman","ilovecatwoman");
-        Account account = new Account(user);
-        account.deposit(5000);
-        item0.purchase(account);
-        assertEquals(account,item0.getAccount());
-    }
-
     @Test
     void purchase() {
         User user = new User("Bruce","batman","ilovecatwoman");
         Account account = new Account(user);
         account.deposit(5000);
-        assertTrue(item0.purchase(account));
-        assertFalse(item1.purchase(account));
+        assertTrue(item0.buy(account));
+        assertFalse(item1.buy(account));
 
     }
-
     @Test
-    void getType() {
-        assertEquals(Item.ItemType.ITEM, item0.getType());
-        assertEquals(Item.ItemType.BILL, item1.getType());
+    void getDatePurchased() {
+        User user = new User("Bruce", "batman", "ilovecatwoman");
+        Account account = new Account(user);
+        account.deposit(5000);
+
+
+        assertEquals(LocalDateTime.MIN, item0.getDatePurchased());
+        assertEquals(LocalDateTime.MIN, item1.getDatePurchased());
+
+        assertTrue(item0.buy(account));
+
+        assertEquals(
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS),
+                item0.getDatePurchased().truncatedTo(ChronoUnit.SECONDS)
+        );
+    }
+    @Test
+    void getAccount() {
+        User user1 = new User("Bruce","batman","ilovecatwoman");
+        User user2 = new User("Bruce","batman","ilovecatwoman");
+        User user3 = new User("Bruce","batman","ilovecatwoman");
+        User user4 = new User("Bruce","batman","ilovecatwoman");
+        Account account = new Account(user4);
+        account.deposit(5000);
+
+        assertTrue(item0.buy(account));
+        assertEquals(account.getNumber(), item0.getAccountNumber());
+
+        assertFalse(item1.buy(account));
+        assertEquals(-1, item1.getAccountNumber());
     }
 }
