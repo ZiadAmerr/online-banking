ffee95c5a3ac6b052c69eb8804936c0ab65905e4
Ahmed Tarek <92989834+AhmedTareek@users.noreply.github.com>
2023-05-12 23:46:41 +0300

    Testing (#6)
    * Added tests for Item Class
    
    * updated the ItemTest and Added UserTest
    
    Co-authored-by: Ziad Amerr <70425741+ZiadAmerr@users.noreply.github.com>

---
+++ source/test/ItemTest.java
@@ -0,0 +1,69 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+class ItemTest {
+    private static Item item0;
+    private static Item item1;
+    @BeforeAll
+    static void init(){
+        item0 = new Item(Item.ItemType.ITEM,5,"pepsi");
+        item1 = new Item(Item.ItemType.BILL,99999,"fees");
+    }
+    @Test
+    void testGetId() {
+        assertEquals(0,item0.getId());
+        assertEquals(1,item1.getId());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("pepsi",item0.getName());
+        assertEquals("fees",item1.getName());
+    }
+
+    @Test
+    void testGetPrice() {
+        assertEquals(5,item0.getPrice());
+        assertEquals(99999,item1.getPrice());
+        assertThrows(IllegalArgumentException.class ,()->
+                new Item(Item.ItemType.ITEM,-23,"water"));
+    }
+
+
+    @Test
+    void getDatePurchased() {
+        User user = new User("Bruce","batman","ilovecatwoman");
+        Account account = new Account(user);
+        account.deposit(5000);
+        item0.purchase(account);
+        assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS),item0.getDatePurchased().truncatedTo(ChronoUnit.SECONDS));
+    }
+
+    @Test
+    void getAccount() {
+        User user = new User("Bruce","batman","ilovecatwoman");
+        Account account = new Account(user);
+        account.deposit(5000);
+        item0.purchase(account);
+        assertEquals(account,item0.getAccount());
+    }
+
+    @Test
+    void purchase() {
+        User user = new User("Bruce","batman","ilovecatwoman");
+        Account account = new Account(user);
+        account.deposit(5000);
+        assertTrue(item0.purchase(account));
+        assertFalse(item1.purchase(account));
+
+    }
+
+    @Test
+    void getType() {
+        assertEquals(Item.ItemType.ITEM, item0.getType());
+        assertEquals(Item.ItemType.BILL, item1.getType());
+    }
+}
