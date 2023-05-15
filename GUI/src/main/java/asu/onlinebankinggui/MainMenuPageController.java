a9998fc07135d633d6e42d5ead4b805259029004
KareemWael1 <87460487+KareemWael1@users.noreply.github.com>
2023-05-15 19:14:17 +0300

    Login and Signup

---
+++ GUI/src/main/java/asu/onlinebankinggui/MainMenuPageController.java
@@ -0,0 +1,17 @@
+package asu.onlinebankinggui;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class MainMenuPageController {
+    @FXML
+    protected void onMyAccountsButtonClick() throws IOException {
+        // TODO Add my accounts fxml
+    }
+
+    @FXML
+    protected void onTransactionsButtonClick() throws IOException {
+        // TODO Add transactions fxml
+    }
+}
