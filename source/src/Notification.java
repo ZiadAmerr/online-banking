36ff78af39f1fba9491e09df5db3710fc2019856
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-05-21 03:44:33 +0300

    Added data getters

--- source/src/Notification.java
+++ source/src/Notification.java
@@ -10,5 +10,16 @@ class Notification {
         this.message = message;
         this.dateSent = LocalDateTime.now();
     }
+
+    /** Returns an array of the notification details
+     * @return The array contains the following elements:
+     *     <ol start="0">
+     *         <li>message - type: String
+     *         <li>dateSent - type: LocalDateTime
+     *     </ol>
+     */
+    public Object[] getData() {
+        return new Object[] {message, dateSent};
+    }
 }
 
--- source/src/Transaction.java
+++ source/src/Transaction.java
@@ -37,4 +37,27 @@ class Transaction {
             return toAccount;
         }
     }
+
+    /** TODO: implement this method
+     * Returns an array of the transaction details
+     * @return The array contains the following elements:
+     * <ol start="0">
+     *   <li>fromAccountNumber - type: int
+     *   <li>amount - type: float
+     *   <li>date - type: LocalDateTime
+     *   <li>toAccount - type: int
+     *   <li>isToItem - type: boolean
+     *   <li>item - type: String
+     */
+    public Object[] getData() {
+        Object[] transactionDetails = new Object[6];
+        transactionDetails[0] = fromAccountNumber;
+        transactionDetails[1] = amount;
+        transactionDetails[2] = date;
+        transactionDetails[3] = toAccount;
+        transactionDetails[4] = isToItem;
+        transactionDetails[5] = item;
+
+        return transactionDetails;
+    }
 }
